5e8d5a9d2e8d8c348f058fc7aa309dd1d6ebcc97
caetan4 <cprieto@centro.edu.mx>
2025-08-21 14:31:14 -0600

    finalclase

---
+++ Ejercicio 03/app.js
@@ -0,0 +1,11 @@
+console.log("Ejercicio 03: Interacción con el canvas");
+console.log(gsap);
+
+gsap.to(
+    ".rectangulo",
+    {
+        x : 100,
+        duration: 5,
+        ease: "power1.inOut",
+ }
+);
